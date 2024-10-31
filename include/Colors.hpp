8ae2de8da57c6a8674721c181f11d58663ccf7f9
Andrey Underoak <eaa4robo@gmail.com>
2024-10-31 17:38:36 +0200

    add color cout

---
+++ include/Colors.hpp
@@ -0,0 +1,10 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET_COLOR   "\033[0m"
+#define RED_COLOR     "\033[31m"
+#define GREEN_COLOR   "\033[32m"
+#define YELLOW_COLOR  "\033[33m"
+#define BLUE_COLOR    "\033[34m"
+
+#endif

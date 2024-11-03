476f17f8da9b1e7e3059565cc0bdbcb0499fc339
Andrey Underoak <eaa4robo@gmail.com>
2024-11-03 17:03:54 +0200

    delete comparator; add cout to recorder

--- utils/tape_recorder.cpp
+++ utils/tape_recorder.cpp
@@ -17,10 +17,12 @@ int main(int argc, char *argv[]){
                     cp.get_delay_full());
     t.go_to(0);
     int num, i = 0;
-    std::cout<<"Вводите числа поочередно с новой строки для записи в ленту: \nЧтобы остановить запись введи любой символ"<<std::endl;
+    std::cout<<"Вводите числа поочередно после слова Вводите: \nЧтобы остановить запись введи любой символ"<<std::endl;
+    std::cout<<"Вводите: "<<std::endl;
     while (std::cin >> num) {
         t.write_value(num);
         t.go_to(++i);
+        std::cout<<"Вводите: "<<std::endl;
     }
     t.clear_after(i);
     return 0;
